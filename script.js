a7c10cbd7b093f4f7dbc373cb8f019d862b23a73
arkarmyomin <145448084+akmm-dev@users.noreply.github.com>
2023-10-08 15:36:20 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 
+
 async function getDream() {
     let response = await fetch('DreamDictionary.json');
     let data = await response.json();
     let dreams = data.BlogDetail;
     let dreamsId = data.BlogHeader;
-    
+    var matchItem;
 
     for (let i = 0; i < dreamsId.length; i++) {
         
@@ -24,18 +25,41 @@ async function getDream() {
             document.querySelector('.body').style.display = 'none';
             document.querySelector('.d-body').style.display = 'block';
             document.querySelector('.back').style.display = 'flex';
+            document.querySelector('.head').style.display = 'none';
+            document.querySelector('.search').style.display = 'flex';
 
             for (let j = 0; j < dreams.length; j++) { 
 
                 if (dreams[j].BlogId === choseId) { 
-                    console.log("hi");
+                    // console.log("hi");
                     document.querySelector('.d-body').innerHTML +=
                         `<div class="d">${dreams[j].BlogContent}</div>`;
-                    
                 }
+                
+                
+
+                
+            }
+            let dream = document.querySelectorAll('.d'); 
+            let search = document.querySelector('#searchBox');
+
+            for (let j = 0; j < dream.length; j++) { 
+                
+                let match = dream[j].innerHTML;
+                search.addEventListener('keyup', () => {
+                    // console.log(search.value);
+                    if (match.indexOf(search.value) > -1) {
+                        dream[j].style.display = '';
+                    } else {
+                        dream[j].style.display = 'none';
+                    }
+                })
+                
             }
         })
+        
     })
+
 }
 
 getDream();
@@ -44,5 +68,8 @@ function showHome() {
     document.querySelector('.body').style.display = 'flex';
     document.querySelector('.d-body').style.display = 'none';
     document.querySelector('.back').style.display = 'none';
-    
+    document.querySelector('.head').style.display = 'flex';
+    document.querySelector('.search').style.display = 'none';
+
+  
 }
